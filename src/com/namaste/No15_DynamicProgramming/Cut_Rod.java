77d25d4874baaa99221ed83c0bbe47ab27ef557a
loozhang <loozhang@163.com>
2021-03-06 16:57:19 +0800

    update

--- src/com/namaste/No15_DynamicProgramming/Cut_Rod.java
+++ src/com/namaste/No15_DynamicProgramming/Cut_Rod.java
@@ -1,12 +1,12 @@
 package com.namaste.No15_DynamicProgramming;
 
 public class Cut_Rod {
-    static int q;
-    static int[] p = new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-    static int[] r = new int[p.length];
+    static int q;//最优价格临时变量
+    static int[] p = new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};//不同长度对应的价格
+    static int[] r = new int[p.length];//初始化不同长度对应的最优价格都为0
 
     public static void main(String[] args) {
-        System.out.printf(getMaxValue(5) + "");
+        System.out.printf(getMaxValue(4) + "");
     }
 
     private static int getMaxValue(int length) {
@@ -19,7 +19,7 @@ public class Cut_Rod {
             return r[n];
         }
         if (n == 0)
-            return q = 0;
+            return 0;
         else
             for (int i = 1; i <= n; i++) {
                 q = Math.max(q, p[i] + memoized_aux(n - i, r, p));
